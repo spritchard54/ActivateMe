a9aacfab8684baa8f4c207a749ea7d199717a52c
Bogdan Tkachuk <cnndo14@gmail.com>
2024-03-07 18:45:33 -0500

    added models imports/exports in models/index.js file

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,11 @@
 const User = require('./User');
+const Category = require('./Category');
+const Activity = require('./Activity');
+const ActivityType = require('./ActivityType');
 
-module.exports = { User };
+module.exports = { 
+    User,
+    Category,
+    Activity,
+    ActivityType
+};
