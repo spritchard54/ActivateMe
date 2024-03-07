13ca9ee8e72d9f48552f437642695e51a6c8feb0
spritchard54 <>
2024-03-07 18:34:05 -0500

    Updates to resolvers, type defs, and server js files.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,15 +2,16 @@ const { User } = require("../models");
 const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
-  Query: {},
+  Query: {
   me: async (parent, args, context) => {
     console.log(context);
     if (context.user) {
-      return User.findOne({ _id: context.user._id }).populate('activities');
+      const userData = await User.findOne({ _id: context.user._id }).populate('activities'); 
+      return userData;
     }
     throw AuthenticationError;
   },
-
+  },
 
   Mutation: {
     addUser: async (parent, { username, email, password }) => {
