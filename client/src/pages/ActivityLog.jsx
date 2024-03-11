519e60f2d4cc127a5c4bcaba904be83089c02e91
spritchard54 <>
2024-03-11 19:56:44 -0400

    Added Activity Log link to header, structure added to Activity Log page, users query added to resolvers

--- client/src/pages/ActivityLog.jsx
+++ client/src/pages/ActivityLog.jsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
+import { Navigate, useParams, Link } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
 
-export default function ActivityLog(){
+export default function ActivityLog() {
+  const { username: userParam } = useParams();
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
+  });
 
-  return(
+  const user = data?.me || data?.user || {};
+  // navigate to personal profile page if username is yours
+  if (Auth.loggedIn() && Auth.getUser().data.username === userParam) {
+    return <Navigate to="/Dashboard" />;
+  }
 
-    <>
-    
-    {/* design code here */}
-    
-    </>
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  )
+  if (!user?.username) {
+    return (
+      <div className="container-fluid w-75 my-5 text-center">
+        <h4>
+          You need to be logged in to see this. Use the navigation links above
+          to sign up or log in!
+        </h4>
+      </div>
+    );
+  }
 
+  return (
+    <div className="container-fluid w-75 mt-4">
+      <h2>Activity Log</h2>
+      <div className="row">
+        <p className="my-4">
+          This should contain a table that displays all data from the activty log. This is different from the dashboard where we only see the 5-10 most recent activities.
+        </p>
+      </div>
+    </div>
+  );
 }
