366ab55bcc84a436de1adcb3b0f61e1fda81b507
Tom Dossman <tdossman.development@gmail.com>
2024-03-12 11:24:44 -0400

    dashboard styled to match figma design

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -42,40 +42,47 @@ const Dashboard = () => {
   }
 
   return (
-    <div className="container-fluid w-75">
-      <div className="row mt-5">
-        <div className="col">
-          <h2 className="my-3 white-text">Dashboard</h2>
-        </div>
-        <div className="col">
-          <button onClick={logButton} className="my-3 logAct">
+
+    <div className="container-fluid d-flex justify-content-center align-items-center my-5">
+
+      <div className="container sub-form p-5 my-5">
+
+        <div className="d-flex justify-content-between align-items-center">
+
+          <h2 className="white-text">Dashboard</h2>
+
+
+          <button onClick={logButton} className="logAct">
             Log Activity
           </button>
+
         </div>
+
+        <div className="container mt-2 py-5 d-flex justify-content-around align-items-center">
+          <div className="text-light">Daily Breakdown</div>
+          <div className="text-light">Weekly Breakdown</div>
+          <div className="text-light">Monthly Breakdown</div>
+        </div>
+
+        <div className="container p-1">
+          <h4 id="recActivities" className="my-3 py-1 text-black">
+            Recent Activities
+          </h4>
+          <div className="card p-4 bg-dark">
+            <p className="text-white">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
+              voluptates tenetur excepturi facilis animi labore, facere beatae
+              reprehenderit odit esse a odio veniam at? Voluptatibus, laboriosam?
+              Accusantium quae voluptatibus sunt. Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Modi dolorem accusantium consequuntur
+              magnam a, optio hic illum placeat tempora dignissimos ullam nobis
+              magni cupiditate voluptatum quibusdam qui, dolor omnis veniam!
+            </p>
+          </div>
+        </div>
+
       </div>
 
-      <div className="row mt-2 py-5">
-        <div className="col">Daily Breakdown</div>
-        <div className="col">Weekly Breakdown</div>
-        <div className="col">Monthly Breakdown</div>
-      </div>
-
-      <div className="row">
-        <h4 id="recActivities" className="my-3 py-1">
-          Recent Activities
-        </h4>
-        <div className="card p-5">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
-            voluptates tenetur excepturi facilis animi labore, facere beatae
-            reprehenderit odit esse a odio veniam at? Voluptatibus, laboriosam?
-            Accusantium quae voluptatibus sunt. Lorem ipsum, dolor sit amet
-            consectetur adipisicing elit. Modi dolorem accusantium consequuntur
-            magnam a, optio hic illum placeat tempora dignissimos ullam nobis
-            magni cupiditate voluptatum quibusdam qui, dolor omnis veniam!
-          </p>
-        </div>
-      </div>
     </div>
   );
 };
