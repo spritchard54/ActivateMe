6111b26bfb292d7e607ad6520808a70a0ee97f51
L.lar <l.lar1109@gmail.com>
2024-03-12 04:47:33 -0400

    change up styling on contact form to match others and adjust footer to stay at the base of page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -11,11 +11,13 @@ export default function Contact(){
   // Function to handle input changes
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
+
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
   // Function to handle form submission
   const handleSubmit = (e) => {
@@ -27,52 +29,73 @@ export default function Contact(){
       email: '',
       message: '',
     });
+
+    setFormSubmitted(true);
+
   };
   
   return(
-    <>
-    <div className="container mt-4">
-          <h2 className="mb-4">Contact Us</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label">Name:</label>
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="mb-3">
-            <label htmlFor="email" className="form-label">Email:</label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
+    <main className="flex-row justify-center my-5">
+      <div className={`col-6 col-lg-5 mx-auto my-4 ${formSubmitted ? 'd-none' : ''}`}> 
+        <div className="card mt-5 mb-3 sub-form d-flex">
+          <h4 className="text-light p-2 mt-3 text-center">Contact Us</h4>
+          <div className="card-body pb-4">
+            <form onSubmit={handleSubmit}>
+              <div className='form-box p-3'>
+                <label htmlFor="loginInput" className='form-label login-input'>
+                  Name
+                </label>
+                <input
+                  className="form-input border mb-3 p-2"
+                  placeholder="Enter your name"
+                  name="name"
+                  type="text"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+                <label htmlFor="loginInput" className='form-label login-input'>
+                  Email
+                </label>
+                <input
+                  className="form-input border d-block mb-3 p-2"
+                  placeholder="email@example.com"
+                  name="email"
+                  type="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+                <label htmlFor="loginInput" className='form-label login-input'>Message</label>
+                <textarea
+                  className="form-input border d-block mb-3 p-2"
+                  placeholder="Enter your message"
+                  name="message"
+                  rows="4"
+                  value={formData.message}
+                  onChange={handleChange}
+                  required
+                ></textarea>
+              </div>
+              <button
+                className="btn btn-block d-block mx-auto mt-3 submit-btn"
+                style={{ cursor: 'pointer' }}
+                type="submit"
+              >
+                Submit
+              </button>
+            </form>
           </div>
-          <div className="mb-3">
-            <label htmlFor="message" className="form-label">Message:</label>
-            <textarea
-              className="form-control"
-              id="message"
-              name="message"
-              rows="4"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            ></textarea>
+        </div>
+      </div>
+      {formSubmitted && (
+        <div className="col-6 col-lg-5 mx-auto my-4">
+          <div className="alert alert-success">
+          <p>Thank you for reaching out, {formData.name}!</p>
+          <p>We will get back to you as soon as possible.</p>
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
-          </form>
-    </div>
-    </>
+        </div>
+      )}
+    </main> 
   );
 }
