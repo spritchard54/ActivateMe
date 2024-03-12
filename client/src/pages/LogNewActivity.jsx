c685df872fa64848dc3cc9a56c3880041c02f794
spritchard54 <>
2024-03-11 21:36:05 -0400

    removed extra line of code

--- client/src/pages/LogNewActivity.jsx
+++ client/src/pages/LogNewActivity.jsx
@@ -41,8 +41,7 @@ export default function LogActivity() {
   });
   
   const { loading: activityTypesLoading, data: activityTypesData } = useQuery(GET_ACTIVITYTYPE);
-  
-  const { loading: activityTypesLoading, data: activityTypesData } = useQuery(GET_ACTIVITYTYPE);
+
 
   // Function to handle form input changes
   const handleInputChange = (e) => {
